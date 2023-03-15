8f9734fcfefdf125faa0ddce435706d35a4a9263
Rafał Wałach <62497415+rav250889@users.noreply.github.com>
2023-03-15 22:08:38 +0100

    Update Head.php

--- classes/Head.php
+++ classes/Head.php
@@ -11,13 +11,13 @@
         }
         public function get_head()
         {
-            echo "<meta charset='utf-8'>";
+	    echo "<head>";
             echo "<title>$this->title</title>";
             echo "<meta name='author' content='Rafał Wałach tel. (48)730 341 343'>";
             echo "<meta name='description' content='Aplikacja pozwala na tworzenie własnego planu treningowego. Posiada możliwość zliczania wykonanych serii treningowych'>";
-            echo "<meta name='viewport' content='width=device-width, initial-scale=1.0'>";
             echo "<link rel='icon' type='image/x-icon' href=''/>";
             echo "<link rel='stylesheet' href='$this->css'>";
+	    echo "</head>";	
         }
         
         public function additional_css($path)
